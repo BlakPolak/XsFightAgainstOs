33d86c2d4e87e57e90061b9bd8db799f11d3d4e2
cj <sigamateusz@gmail.com>
2017-06-12 18:36:34 +0200

    Add constructor & 3 fields to Cell class

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,3 +1,12 @@
 
 public class Cell {
+    private final Integer row;
+    private final Integer column;
+    private Seed content;
+
+    public Cell(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+        this.content = Seed.EMPTY;
+    }
 }
