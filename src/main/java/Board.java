90df644a799687d6aaa933fb4b8635e131befa73
cj <sigamateusz@gmail.com>
2017-06-13 11:05:28 +0200

    Create init() for Board

---
+++ src/main/java/Board.java
@@ -0,0 +1,13 @@
+
+public class Board {
+    private Cell[][] cells;
+
+    public void init() {
+        cells = new Cell[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                cells[row][col] = new Cell(row, col);
+            }
+        }
+    }
+}
