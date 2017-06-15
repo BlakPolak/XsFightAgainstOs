60abca16aa69c2a3aa89c0c63b17d1408e60a80c
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-16 00:52:15 +0200

    Refactor and clean code

--- src/test/java/view/UITest.java
+++ src/test/java/view/UITest.java
@@ -1,6 +1,6 @@
 package view;
-
 import model.Seed;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -34,75 +34,82 @@ class UITest {
         String toPrintSting = "Simple text";
         String expectedString = "Simple text\n\n";
         UI.printText(toPrintSting);
+
         assertEquals(expectedString, outContent.toString());
     }
 
     @Test
-    @DisplayName("Prepare Welcome Text Returns String Welcome In Tic Tac Toe Game")
+    @DisplayName("Prepare Welcome Text returns string 'Welcome In Tic Tac Toe Game'")
     void testIfPrepareWelcomeTextReturnsExpectedString() {
         String expectedString = "Welcome in Tic Tac Toe game!";
         String actualString = UI.prepareWelcomeText();
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Which Player Starts Text Returns String 'Player X/O will be the first player this round!'")
+    @DisplayName("Prepare Which Player Starts Text returns string 'Player 'X/O' will be the first player this round!'")
     void testIfPrepareWhichPlayerStartsTextReturnsExpectedString() {
         String expectedString = "Player  'X'  will be the first player this round!";
         Seed player = Seed.CROSS;
         String actualString = UI.prepareWhichPlayerStartsText(player);
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Wrong Argument Text Returns String 'This move is not valid. Try again...'")
+    @DisplayName("Prepare Wrong Argument Text returns string 'This move is not valid. Try again...'")
     void testIfPrepareWrongArgumentTextReturnsExpectedString() {
         String expectedString = "This move is not valid. Try again...";
         String actualString = UI.prepareWrongArgumentText();
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Which Player Won Text Returns String 'Player X/O won the game!'")
+    @DisplayName("Prepare Which Player Won Text returns string 'Player 'X/O' won the game!'")
     void testIfPrepareWhichPlayerWonTextReturnsExpectedString() {
         String expectedString = "Player 'O' won the game!";
         Seed wonPlayer = Seed.NOUGHT;
         String actualString = UI.prepareWhichPlayerWonText(wonPlayer);
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Play Again Text Returns String 'Would you like to play again? [y/n]'")
+    @DisplayName("Prepare Play Again Text returns string 'Would you like to play again? [y/n]'")
     void testIfPreparePlayAgainTextReturnsExpectedString() {
         String expectedString = "Would you like to play again? [y/n]";
         String actualString = UI.preparePlayAgainText();
+
         assertEquals(expectedString, actualString);
     }
 
-
     @Test
-    @DisplayName("Prepare Which Players Turn Text Returns String 'Player 'X/O', enter your move (row[1-3], column[1-3]):'")
+    @DisplayName("Prepare Which Players Turn Text returns string 'Player 'X/O', enter your move (row[1-3], column[1-3]):'")
     void testWhichPlayersTurnTextReturnsExpectedString() {
-        String expectedString = "Player 'X', enter your move (row[1-3], column[1-3]): ";
         Seed currentPlayer = Seed.CROSS;
+        String expectedString = "Player 'X', enter your move (row[1-3], column[1-3]): ";
         String actualString = UI.prepareWhichPlayersTurnText(currentPlayer);
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Draw Text Returns String 'Unfortunately no one won - there is a draw!'")
+    @DisplayName("Prepare Draw Text returns string 'Unfortunately no one won - there is a draw!'")
     void testIfPrepareDrawTextReturnsExpectedString() {
         String expectedString = "Unfortunately no one won - there is a draw!";
         String actualString = UI.prepareDrawText();
+
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    @DisplayName("Prepare Cell Is Occupied Text Returns String 'Unfortunately no one won - there is a draw!'")
+    @DisplayName("Prepare Cell Is Occupied Text returns string 'Unfortunately no one won - there is a draw!'")
     void testIfPrepareCellIsOccupiedTextReturnsExpectedString() {
         String expectedString = "Sorry, but this field is already occupied, select another empty field";
         String actualString = UI.prepareCellIsOccupiedText();
+
         assertEquals(expectedString, actualString);
     }
-
 }
