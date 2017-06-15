86096bf2601cccb100627862222180a1f6dd318a
cj <sigamateusz@gmail.com>
2017-06-15 19:10:50 +0200

    Add updateGameState() to GameController

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -30,8 +30,11 @@ public class GameController {
             String whichPlayersTurn = prepareWhichPlayersTurn(newGame.getCurrentPlayer());
             printText(whichPlayersTurn);
             ArrayList rowAndColumn = takeUserInput();
+            Integer selectedRow = (Integer) rowAndColumn.get(0);
+            Integer selectedColumn = (Integer) rowAndColumn.get(1);
             System.out.println(rowAndColumn.get(0));
             System.out.println(rowAndColumn.get(1));
+            newGame.updateGameState(selectedRow, selectedColumn);
         }
     }
 }
