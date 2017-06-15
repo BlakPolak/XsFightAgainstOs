bea58bad791af4308803ceea240d3685198640a8
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-15 20:10:54 +0200

    Create testPrepareWhichPlayerStartsTextReturnsExpectedString()

--- src/test/java/view/UITest.java
+++ src/test/java/view/UITest.java
@@ -1,5 +1,6 @@
 package view;
 
+import model.Seed;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,8 +29,8 @@ class UITest {
     }
 
     @Test
-    @DisplayName("PrintText prints the same text as it takes")
-    void testPrintTextPrintsTheSameTextAsItTakes() {
+    @DisplayName("PrintText prints the text as expected")
+    void testPrintTextPrintsTextInExpectedWay() {
         String toPrintSting = "Simple text";
         String expectedString = "Simple text\n\n";
         UI.printText(toPrintSting);
@@ -38,9 +39,18 @@ class UITest {
 
     @Test
     @DisplayName("Prepare Welcome Text Returns String Welcome In Tic Tac Toe Game")
-    void testPrepareWelcomeTextReturnsStringWelcomeInTicTacToeGame() {
+    void testPrepareWelcomeTextReturnsExpectedString() {
         String expectedString = "Welcome in Tic Tac Toe game!";
         String actualString = UI.prepareWelcomeText();
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    @DisplayName("Prepare Which Player Starts Text Returns String 'Player X/O will be the first player this round!'")
+    void testPrepareWhichPlayerStartsTextReturnsExpectedString() {
+        String expectedString = "Player  'X'  will be the first player this round!";
+        Seed player = Seed.CROSS;
+        String actualString = UI.prepareWhichPlayerStartsText(player);
         assertEquals(expectedString, actualString);
     }
 }
